932f826c0d7e69fa33706cb1170b152756595b88
Reece Giles <dev@reecegiles.com>
2022-06-22 16:30:39 +0100

    Updates

--- src/events/types/order-status.ts
+++ src/events/types/order-status.ts
@@ -1,6 +1,6 @@
 export enum OrderStatus {
     // When the order has been created, but the ticket it is trying to order has not been reserved.
-    Create = 'created',
+    Created = 'created',
     // The ticket the order is trying to reserve has already been reserved, or when the user has cancelled the order.
     // The Order expires before payment
     Cancelled = 'cancelled',
